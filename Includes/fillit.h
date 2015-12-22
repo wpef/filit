5a50e9202cbe074a38a6c958234112240a90b4a4
Foulques DE MONCADE <fde-monc@e3r5p9.42.fr>
2015-12-22 21:52:11 +0100

    +ft_read file sorti

--- Includes/fillit.h
+++ Includes/fillit.h
@@ -6,15 +6,26 @@
 /*   By: fde-monc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/21 17:00:41 by fde-monc          #+#    #+#             */
-/*   Updated: 2015/12/21 17:55:48 by fde-monc         ###   ########.fr       */
+/*   Updated: 2015/12/22 16:11:55 by fde-monc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-typedef struct { 
-	char	tetrim[4][5];
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include "libft.h"
+# include <fcntl.h>
+# include <stdio.h>
+
+typedef struct {
+	char	ltr;
+	int		coord[4][2];
 }			t_tris;
 
-t_tris	*ft_maketab(char *buf);
-int	ft_isvalid(char *buf);
-int ft_touch(char *buf);
+t_tris		**ft_createtab(char *file);
+t_tris		*ft_maketris(char *buf);
+int			ft_touch(char *buf);
+int			ft_isvalid(char *buf);
+int			ft_readfile(char *file);
 
+#endif
